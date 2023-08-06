42257b39e5671e90c52932db7d20eaed93076c60
laura <laura.daguerds@gmail.com>
2023-08-06 14:22:00 -0300

    Herança Exercicios

---
+++ src/semana17/Ex02/ContaSalario.java
@@ -0,0 +1,27 @@
+package semana17.Ex02;
+
+import java.time.LocalDate;
+
+public class ContaSalario extends Conta {
+    
+    private Empresa empresa;
+
+    public ContaSalario (int numero, LocalDate dataAbertura, double saldo, double tarifa, Correntista correntista, Empresa empresa) {
+        super(numero, dataAbertura, saldo, tarifa, correntista);
+        this.empresa = empresa;
+    }
+
+    public Empresa getEmpresa() {
+        return this.empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public double calcularTarifa() {
+        return tarifa;
+    }
+
+}
